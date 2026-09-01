d219c34d1be0ee750c226d98ef7719da8e89db55
Laura Sampaio Neves <sampaiolaura55@gmail.com>
2026-08-31 21:34:15 -0300

    feat(style): instalação tailwindcss

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -2,9 +2,18 @@
 
 const Header = () => {
   return (
-    <div>
-      <p>Navbar</p>
-    </div>
+    <header className=''>
+        <nav className='flex flex-col md:flex-row justify-around items-center my-3'>
+            <span>EcoTrend</span>
+            <ul className='flex flex-col md:flex-row gap-4'>
+                <li>Geral</li>
+                <li>Em promoção</li>
+                <li>Coleções</li>
+                <li>Reviews</li>
+            </ul>
+            <span>Carrinho</span>
+        </nav>
+    </header>
   )
 }
 
--- src/components/template/Grid.jsx
+++ src/components/template/Grid.jsx
@@ -2,18 +2,9 @@ import React from 'react'
 
 const Grid = () => {
   return (
-    <header>
-        <nav>
-            <span>EcoTrend</span>
-            <ul>
-                <li>Geral</li>
-                <li>Em promoção</li>
-                <li>Coleções</li>
-                <li>Reviews</li>
-            </ul>
-            <span>Carrinho</span>
-        </nav>
-    </header>
+    <div>
+        <p>grid</p>
+    </div>
   )
 }
 
